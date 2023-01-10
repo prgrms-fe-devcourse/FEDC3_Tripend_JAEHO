4166e1b7fe9684687a912579a690bc44d23b7407
김성현 <shkim061198@gmail.com>
2023-01-10 15:13:34 +0900

    fix: 에러 수정

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,13 +1,13 @@
 import ChannelList from '../../components/ChannelList';
 import SideBarRight from '../../components/sidebarRIght';
-import Posts from '../../components/Posts';
+import MainSection from '../../components/MainSection';
 import * as style from './style';
 
 const MainPage = () => {
   return (
     <style.MainPageContainer>
       <ChannelList />
-      <Posts />
+      <MainSection />
       <SideBarRight />
     </style.MainPageContainer>
   );

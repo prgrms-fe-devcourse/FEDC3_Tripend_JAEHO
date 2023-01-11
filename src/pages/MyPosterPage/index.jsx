f5d0e2f550a51d36e6811765e81703f99c3372dd
Seung-hwan285 <ghks123285@gmail.com>
2023-01-11 22:32:32 +0900

    refactor : 안쓰는 코드 제거

--- src/pages/MyPosterPage/index.jsx
+++ src/pages/MyPosterPage/index.jsx
@@ -7,15 +7,11 @@ import { userLoginState } from '../../recoil/auth';
 const MyPosterPage = () => {
   const isLogin = useRecoilValue(userLoginState);
 
-  console.log(isLogin);
   return (
     <>
       <UserInfo />
       <UserPoster />
       <Sidebar />
-
-      {/*  여기서 슬라이드바를 관리하면 작성한글, 내정보수정 페이지로 각각 이동해서
-      렌더링되었을때 없어지게 된다.*/}
     </>
   );
 };

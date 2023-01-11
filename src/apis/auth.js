15f359e04342c29f259efc478f69960a5870329f
Hwan <74364667+Seung-hwan285@users.noreply.github.com>
2023-01-11 19:09:09 +0900

    Revert "Feat/password"

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,7 +1,7 @@
-import { authRequest, baseRequest } from './core';
+import { baseRequest } from './core';
 import swal from 'sweetalert';
 import { getStorage, setStorage } from '../utils/storage';
-import { ERROR_MESSAGE_AUTH, ID, TOKEN, URL, USER as AUTH, USER } from '../utils/auth/constant';
+import { AUTH, BEARER, ERROR_MESSAGE, TOKEN, URL, USER } from '../utils/auth/constant';
 import { ERROR_MESSAGE_SIGNUP } from '../utils/auth/constant';
 
 const { DUPLICATE_EMAIL } = ERROR_MESSAGE_SIGNUP;
@@ -14,23 +14,19 @@ export const postUserLogin = async (email, password) => {
       password,
     })
     .catch(() => {
-      swal(AUTH.LOGIN_FAILED, USER.ID_PASSWORD, ERROR_MESSAGE_AUTH.LOGIN_ERROR);
+      swal(AUTH.LOGIN_FAILED, USER.ID_PASSWORD, ERROR_MESSAGE.AUTH_ERROR);
     });
 
   setStorage(TOKEN, response.data.token);
-  setStorage(ID, response.data.user._id);
   return response;
 };
 
 // 로그인 유저 정보 가져오기 (인증된 유저)
 export const getUser = async () => {
-  return await authRequest.get(URL.AUTH_USER);
-};
-
-// 비밀번호 변경
-export const putPaswwordChange = async (password) => {
-  return await authRequest.put(URL.PASSWORD_UPADTE, {
-    password,
+  return await baseRequest.get(URL.AUTH_USER, {
+    headers: {
+      Authorization: `${BEARER} ${getStorage(TOKEN)}`,
+    },
   });
 };
 

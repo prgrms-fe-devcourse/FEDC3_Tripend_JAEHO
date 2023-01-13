9b5d02cf433b5148b7180492cc659b8a3a5f62e2
sdwoo <damjang67@naver.com>
2023-01-13 18:29:14 +0900

    feat: 알람 모달에 이미지 없을시 처리

--- src/components/alarm/AlarmPopupItem/index.jsx
+++ src/components/alarm/AlarmPopupItem/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { extractName } from '../../../utils/validate/userList';
+import Avatar from '../../common/Avatar';
 import { AlarmItem, AlarmImg, AlarmText } from './style';
 
 const AlarmPopupItem = ({ author, onClick }) => {
   const alarmCategory = author && author.comments ? '댓글' : '좋아요';
   const filteredName = extractName.exec(author.fullName)[0];
-  const alarmComment = `${filteredName}님이 회원님의 게시물에 ${alarmCategory}를 남겼습니다`;
+  const alarmComment = `${filteredName}님이 회원님의 게시물에 ${alarmCategory}을 남겼습니다`;
 
   return (
     author && (
       <AlarmItem onClick={onClick}>
-        <AlarmImg src={author.image} />
+        <AlarmImg>
+          <Avatar shape="circle" size={25} src={author.image} lazy={true} threshold={0.1} />
+        </AlarmImg>
         <AlarmText>{alarmComment}</AlarmText>
       </AlarmItem>
     )

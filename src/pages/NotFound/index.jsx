e10916d5a04ffdbf4ac6ab846bd7cee14a8bd9fd
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-01-14 18:28:00 +0900

    feat: NotFound 페이지 구현

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,8 +1,16 @@
-import { Navigate } from 'react-router-dom';
+import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
+import { MissingPageContainer, MainPageLink, MissingMessage } from './style.js';
+
 const MissingPage = () => {
   return (
     <>
-      <Navigate to="/" replace />
+      <MissingPageContainer>
+        <MissingMessage>
+          <SentimentDissatisfiedIcon />
+          404 NOT FOUND
+        </MissingMessage>
+        <MainPageLink href="/main">Do you want to go to the main page? click me!</MainPageLink>
+      </MissingPageContainer>
     </>
   );
 };

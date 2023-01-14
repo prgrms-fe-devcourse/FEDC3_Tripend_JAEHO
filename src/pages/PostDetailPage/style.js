87e69eb793bd5f24e1aab2c0f510da92ece0635f
sdwoo <damjang67@naver.com>
2023-01-14 23:42:39 +0900

    feat: PostDetailPage 구현

---
+++ src/pages/PostDetailPage/style.js
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const PostDetailPageWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  background-color: var(--gray);
+  justify-content: center;
+  box-sizing: border-box;
+`;
+
+export const PostDetailPageContainer = styled.div`
+  width: 80%;
+  height: 80%;
+  background-color: var(--white);
+  border-radius: 8px;
+`;

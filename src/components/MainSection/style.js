36890711a6f015dd8fb70a6fd04932d5be2a2327
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-01-15 18:28:23 +0900

    feat: 포스트가 없는 경우의 MainSection 화면 구현

--- src/components/MainSection/style.js
+++ src/components/MainSection/style.js
@@ -9,3 +9,14 @@ export const PostsContainer = styled.div`
     display: none;
   }
 `;
+export const NotFoundResultContainer = styled.div`
+  width: 500px;
+  height: 50px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  font-size: 20px;
+`;

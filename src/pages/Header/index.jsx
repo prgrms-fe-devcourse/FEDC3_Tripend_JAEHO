d5709cff143b98c73cbe24045b25d0dc23e408b8
sdwoo <damjang67@naver.com>
2023-01-16 16:26:04 +0900

    refactor: private header 제거

--- src/pages/Header/index.jsx
+++ src/pages/Header/index.jsx
@@ -17,7 +17,6 @@ import {
 } from './style';
 import { getStorage, setStorage } from '../../utils/storage';
 import { TOKEN, USERIMAGE } from '../../utils/constant/auth';
-import PrivateHeader from './PrivateHeaderContainer';
 import SearchPost from '../../components/SearchPost';
 import AddPost from '../../components/addPost';
 import Icon from '../../components/common/Icons';
@@ -37,8 +36,7 @@ const Header = () => {
   const userImage = getStorage(USERIMAGE);
 
   const handleClickLogo = () => {
-    // isLogin ? navigate('/main') : navigate('/');
-    navigate('/main');
+    getToken ? navigate('/main') : navigate('/');
   };
 
   const handleAlarmOpen = async ({ target }) => {
@@ -68,44 +66,40 @@ const Header = () => {
   };
 
   return (
-    <>
-      {getToken ? (
-        <HeaderContainer>
-          <LogoContaniner onClick={handleClickLogo}>
-            <Logo />
-          </LogoContaniner>
+    <HeaderContainer>
+      <LogoContaniner onClick={handleClickLogo}>
+        <Logo />
+      </LogoContaniner>
 
-          <>
-            <SearchPost />
-            <ButtonContainer>
-              <IconItem onClick={handleOpenAddPostModal}>
-                <Icon.AddPostIcon />
+      {getToken && (
+        <>
+          <SearchPost />
+          <ButtonContainer>
+            <IconItem onClick={handleOpenAddPostModal}>
+              <Icon.AddPostIcon />
+            </IconItem>
+            <AlarmContainer>
+              <IconItem onClick={handleAlarmOpen}>
+                <Icon.Alarm />
               </IconItem>
-              <AlarmContainer>
-                <IconItem onClick={handleAlarmOpen}>
-                  <Icon.Alarm />
-                </IconItem>
-              </AlarmContainer>
-              <IconItem onClick={handleLogout}>
-                <Icon.LogoutIcon />
-              </IconItem>
-              <IconItem onClick={handleOpenMyPage}>
-                <Avatar shape="circle" size="30px" src={userImage} lazy={true} threshold={0.1} />
-              </IconItem>
-            </ButtonContainer>
-            {isVisibleModal && <AddPost visible={isVisibleModal} />}
-            <AlarmPopup
-              visible={isAlarmOpen}
-              onClose={handleAlarmClose}
-              target={alarmBox}
-              alarms={alarms}
-            />
-          </>
-        </HeaderContainer>
-      ) : (
-        <PrivateHeader />
+            </AlarmContainer>
+            <IconItem onClick={handleLogout}>
+              <Icon.LogoutIcon />
+            </IconItem>
+            <IconItem onClick={handleOpenMyPage}>
+              <Avatar shape="circle" size="30px" src={userImage} lazy={true} threshold={0.1} />
+            </IconItem>
+          </ButtonContainer>
+          {isVisibleModal && <AddPost visible={isVisibleModal} />}
+          <AlarmPopup
+            visible={isAlarmOpen}
+            onClose={handleAlarmClose}
+            target={alarmBox}
+            alarms={alarms}
+          />
+        </>
       )}
-    </>
+    </HeaderContainer>
   );
 };
 

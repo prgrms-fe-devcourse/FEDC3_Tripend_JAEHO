e03a026bc229f1f8fee11ff59d080d6386df0179
sdwoo <damjang67@naver.com>
2023-01-16 17:54:19 +0900

    fix: 검색 결과 오류 해결

--- src/utils/validate/searchedPostLIst.js
+++ src/utils/validate/searchedPostLIst.js
@@ -1,10 +1,8 @@
-import { extractName } from './userList';
-
 export const filterdPost = (list) => {
   return list
     .filter((item) => item.title)
     .map((item) => {
-      item.title = extractName.exec(item.title)[0];
+      item.title = JSON.parse(item.title).title;
       return item;
     })
     .splice(0, 10);

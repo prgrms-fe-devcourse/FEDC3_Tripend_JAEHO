8b5723c3f26a6b1eb31874004c557c9923c246f2
sdwoo <damjang67@naver.com>
2023-01-16 13:49:39 +0900

    fix: 알람 메시지 수정

--- src/components/alarm/AlarmPopupItem/index.jsx
+++ src/components/alarm/AlarmPopupItem/index.jsx
@@ -4,8 +4,9 @@ import { extractName } from '../../../utils/validate/userList';
 import Avatar from '../../common/Avatar';
 import { AlarmItem, AlarmImg, AlarmText } from './style';
 
-const AlarmPopupItem = ({ author, onClick }) => {
-  const alarmCategory = author && author.comments ? ALARM.COMMENT : ALARM.ACCOMPANY;
+const AlarmPopupItem = ({ alarm, onClick }) => {
+  const { author } = alarm;
+  const alarmCategory = alarm && alarm.comments ? ALARM.COMMENT : ALARM.ACCOMPANY;
   const filteredName = extractName.exec(author.fullName)[0];
   const alarmComment = `${filteredName}님이 회원님의 게시물에 ${alarmCategory} 남겼습니다`;
 

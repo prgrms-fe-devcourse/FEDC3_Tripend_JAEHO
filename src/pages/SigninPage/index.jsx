05bc6666ccfe3aca17cca859117d36d4ea5e1577
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-01-17 16:20:23 +0900

    fix: 로그인 상태에서 루트로 이동할 경우 메인 페이지로 리다이렉트 되도록 수정

--- src/pages/SigninPage/index.jsx
+++ src/pages/SigninPage/index.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Login from '../../components/Signin/';
+import { getStorage } from '../../utils/storage';
 
 const SigninPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (getStorage('Token')) {
+      navigate('/main');
+    }
+  }, []);
+
   return (
     <>
       <Login />

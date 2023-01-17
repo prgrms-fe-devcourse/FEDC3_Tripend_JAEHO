a7a62e497f372f93736c7619f316cf94776b2206
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-01-17 14:57:06 +0900

    refactor: usePostForm hook으로 함수 및 상태 분리

--- src/components/addPost/addPostForm/index.jsx
+++ src/components/addPost/addPostForm/index.jsx
@@ -1,11 +1,6 @@
-import { useEffect, useRef, useState } from 'react';
-import { useSetRecoilState } from 'recoil';
-import { isVisibleModalState } from '../../../recoil/addPostStates';
-import { imageToBinary } from '../../../utils/imageConverter';
-import { getStorage } from '../../../utils/storage';
-import { createPost, getChannels } from '../../../apis/post';
+import { useEffect, useState } from 'react';
+import { getChannels } from '../../../apis/post';
 
-import UploadIcon from '../../../../static/images/upload.svg';
 import {
   Description,
   FormContainer,
@@ -20,26 +15,22 @@ import {
   UploadedImage,
 } from './style';
 
-const GenderData = {
-  male: '남자만',
-  female: '여자만',
-  both: '남여 무관',
-};
+import UploadIcon from '../../../../static/images/upload.svg';
+import usePostForm from '../../../hooks/usePostForm';
+import ErrorText from './ErrorText';
 
 const AddPostForm = () => {
-  const setIsVisibleModal = useSetRecoilState(isVisibleModalState);
-  const imageFileInputRef = useRef('');
-  const [imageSrc, setImageSrc] = useState('');
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState('');
-  const [channelId, setChannelId] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [personnel, setPersonnel] = useState(1);
-  const [gender, setGender] = useState('');
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  const {
+    imageSrc,
+    values,
+    isLoading,
+    errorMessage,
+    handleValueChange,
+    handleImageFileChange,
+    handleCountryChange,
+    handleSubmit,
+  } = usePostForm();
 
   // ChannelList 컴포넌트 내부 getChannelData 함수와 중복 => 분리
   const getChannelData = async () => {
@@ -63,94 +54,10 @@ const AddPostForm = () => {
     getChannelData();
   }, []);
 
-  const handleImageFileChange = async (e) => {
-    let imageFile = e.target.files[0];
-
-    if (!imageFile) {
-      return;
-    }
-
-    const reader = new FileReader();
-    reader.readAsDataURL(imageFile);
-    reader.onload = () => {
-      setImageSrc(reader.result);
-    };
-  };
-
-  const handleCountryChange = (e) => {
-    const { country } = e.target.options[e.target.selectedIndex].dataset;
-    setCountry(country);
-    setChannelId(e.target.value);
-  };
-
-  const handleStartDateChange = (e) => {
-    setStartDate(e.target.value);
-  };
-
-  const handleEndDateChange = (e) => {
-    setEndDate(e.target.value);
-  };
-
-  const handlePersonnelChange = (e) => {
-    setPersonnel(e.target.value);
-  };
-
-  const handleGenderChange = (e) => {
-    setGender(e.target.value);
-  };
-
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!getStorage('Token')) {
-      return;
-    }
-
-    if (!country || !channelId || !title || !startDate || !endDate || !personnel || !gender) {
-      return;
-    }
-
-    setIsLoading(true);
-
-    const userData = {
-      country,
-      date: `${startDate} ~ ${endDate}`,
-      personnel,
-      gender: GenderData[gender],
-      title,
-      content,
-    };
-
-    const binaryImage = imageSrc ? imageToBinary(imageSrc) : null;
-
-    const data = {
-      title: JSON.stringify(userData),
-      image: binaryImage,
-      channelId,
-    };
-
-    const formData = new FormData();
-    Object.keys(data).forEach((key) => formData.append(key, data[key]));
-
-    await createPost(formData);
-
-    setIsLoading(false);
-    setIsVisibleModal(false);
-  };
-
   return (
     <PostForm onSubmit={handleSubmit}>
       <ImageUploader>
         <ImageFileInput
-          ref={imageFileInputRef}
           type="file"
           accept="image/png, image/jpeg, image/jpg"
           onChange={handleImageFileChange}
@@ -158,7 +65,7 @@ const AddPostForm = () => {
         <ImageFileContent>
           {imageSrc ? (
             <UploadedImage>
-              <img src={imageSrc} alt="사진" />
+              <img src={imageSrc} alt="userImage" />
             </UploadedImage>
           ) : (
             <>
@@ -172,7 +79,12 @@ const AddPostForm = () => {
       <FormContainer>
         <InputWrapper>
           <label htmlFor="country">나라</label>
-          <select id="country" value={channelId} onChange={handleCountryChange}>
+          <select
+            id="country"
+            name="country"
+            value={values.channelId}
+            onChange={handleCountryChange}
+          >
             {countries.map(({ name, _id }) => (
               <option key={_id} value={_id} data-country={name}>
                 {name}
@@ -186,14 +98,15 @@ const AddPostForm = () => {
             <input
               type="number"
               id="personnel"
-              value={personnel}
+              name="personnel"
+              value={values.personnel}
               min="1"
-              onChange={handlePersonnelChange}
+              onChange={handleValueChange}
             />
           </InputWrapper>
           <InputWrapper>
             <label htmlFor="gender">원하는 성별</label>
-            <select id="gender" value={gender} onChange={handleGenderChange}>
+            <select id="gender" name="gender" value={values.gender} onChange={handleValueChange}>
               <option value={null}>=== 선택 ===</option>
               <option value="male">남자만</option>
               <option value="female">여자만</option>
@@ -204,9 +117,21 @@ const AddPostForm = () => {
         <InputWrapper>
           <label htmlFor="date">기간</label>
           <InputsAlign>
-            <input type="date" id="date" value={startDate} onChange={handleStartDateChange} />
+            <input
+              type="date"
+              id="date"
+              name="startDate"
+              value={values.startDate}
+              onChange={handleValueChange}
+            />
             부터
-            <input type="date" id="date" value={endDate} onChange={handleEndDateChange} />
+            <input
+              type="date"
+              id="date"
+              name="endDate"
+              value={values.endDate}
+              onChange={handleValueChange}
+            />
             까지
           </InputsAlign>
         </InputWrapper>
@@ -215,8 +140,9 @@ const AddPostForm = () => {
           <input
             type="text"
             id="title"
-            value={title}
-            onChange={handleTitleChange}
+            name="title"
+            value={values.title}
+            onChange={handleValueChange}
             placeholder="제목을 입력해주세요."
           />
         </InputWrapper>
@@ -224,12 +150,21 @@ const AddPostForm = () => {
           <label htmlFor="content">내용</label>
           <textarea
             id="content"
-            value={content}
-            onChange={handleContentChange}
+            name="content"
+            value={values.content}
+            onChange={handleValueChange}
             placeholder="내용을 입력해주세요."
           />
         </InputWrapper>
-        <SubmitButton>{isLoading ? '등록 중...' : '등록'}</SubmitButton>
+        <SubmitButton>
+          {!isLoading && errorMessage.length ? (
+            <ErrorText message={errorMessage[0]} />
+          ) : isLoading ? (
+            '등록 중...'
+          ) : (
+            '등록'
+          )}
+        </SubmitButton>
       </FormContainer>
     </PostForm>
   );

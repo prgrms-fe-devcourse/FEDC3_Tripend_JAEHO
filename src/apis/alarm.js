ad383fac8e874722655f4630caa987a4c4779465
sdwoo <damjang67@naver.com>
2023-01-19 16:03:53 +0900

    fix:apis 주석 정리

--- src/apis/alarm.js
+++ src/apis/alarm.js
@@ -1,7 +1,6 @@
 import { URL } from '../utils/constants/alarm';
 import { authRequest } from './core';
 
-// 나의 알림 목록
 export const getMyAlarms = async () => {
   try {
     return await authRequest.get(URL.GET_ALARM);
@@ -10,7 +9,6 @@ export const getMyAlarms = async () => {
   }
 };
 
-// 알림 생성하기
 export const createAlarm = async (notificationType, notificationTypeId, userId, postId) => {
   try {
     return await authRequest.post(URL.CREATE_ALARM, {
--- src/apis/user.js
+++ src/apis/user.js
@@ -1,6 +1,6 @@
 import { USER_URL } from '../utils/constants/user';
 import { baseRequest } from './core';
-// 전체 유저 정보 가져오기
+
 export const getUsers = async () => {
   try {
     const data = await baseRequest.get(USER_URL.GET_ALL_USER);
@@ -10,7 +10,6 @@ export const getUsers = async () => {
   }
 };
 
-// 검색된 유저 정보 가져오기
 export const getUserInfo = async (keyword) => {
   try {
     const data = await baseRequest.get(USER_URL.GET_SEARCHED_USER + keyword);
@@ -20,7 +19,6 @@ export const getUserInfo = async (keyword) => {
   }
 };
 
-// 클릭된 유저 정보 가져오기
 export const getClickedUserInfo = async (userId) => {
   try {
     const data = await baseRequest.get(USER_URL.GET_CLICKED_USER + userId);

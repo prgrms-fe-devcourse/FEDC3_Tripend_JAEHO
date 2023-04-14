efadd4d11872ee6a9f8873b84a971d4aedb85f59
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-04-14 19:13:31 +0900

    fix: 환경 변수 수정 및 설정

--- src/apis/core/index.js
+++ src/apis/core/index.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 import { BEARER, TOKEN } from '../../utils/constants/auth';
 import { getStorage } from '../../utils/storage';
 
-const baseURL =
-  process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_ENDPOINT : '/api';
+const baseURL = process.env.NODE_ENV === 'development' ? import.meta.env.VITE_API_ENDPOINT : '/api';
 
 const baseRequest = axios.create({
   baseURL,

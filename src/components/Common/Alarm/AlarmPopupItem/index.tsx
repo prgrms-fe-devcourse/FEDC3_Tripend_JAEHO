f66f2c9a1d2a13ed1602cb6bfb0fac7655a0d0ee
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-04-27 15:08:46 +0900

    refactor: AlarmPopupItem migration

--- src/components/Common/Alarm/AlarmPopupItem/index.tsx
+++ src/components/Common/Alarm/AlarmPopupItem/index.tsx
@@ -1,24 +1,49 @@
+import Avatar from '@/components/Common/Avatar';
 import { ALARM } from '@/utils/constants/alarm';
 import { extractName } from '@/utils/validate/userList';
-import Avatar from '@/components/Common/Avatar';
 import { AlarmImg, AlarmItem, AlarmText } from './style';
+import { MouseEventHandler } from 'react';
 
-interface AlarmPopupItem {
-  alarm: string[];
-  onClick: void;
+interface AlarmPopupItem extends Alarm {
+  alarm: Alarm;
+  onClick: MouseEventHandler<HTMLElement>;
+}
+
+interface Alarm extends Author {
+  author: Author;
+  comment: string;
+}
+
+interface Author {
+  fullName: string;
+  image: string;
 }
 
 const AlarmPopupItem = ({ alarm, onClick }: AlarmPopupItem) => {
   const { author } = alarm;
   const alarmCategory = alarm && alarm.comment ? ALARM.COMMENT : ALARM.ACCOMPANY;
-  const filteredName = extractName.exec(author.fullName)[0];
-  const alarmComment = `${filteredName}님이 회원님의 게시물에 ${alarmCategory} 남겼습니다`;
+
+  let result;
+  let alarmComment;
+
+  while ((result = extractName?.exec(author.fullName)) !== null) {
+    alarmComment = `${result[0]}님이 회원님의 게시물에 ${alarmCategory} 남겼습니다`;
+  }
 
   return (
     author && (
       <AlarmItem onClick={onClick}>
         <AlarmImg>
-          <Avatar shape="circle" size={25} src={author.image} lazy={true} threshold={0.1} />
+          <Avatar
+            shape="circle"
+            size={25}
+            src={author.image}
+            lazy={true}
+            threshold={0.1}
+            placeholder={''}
+            alt={''}
+            mode={undefined}
+          />
         </AlarmImg>
         <AlarmText>{alarmComment}</AlarmText>
       </AlarmItem>

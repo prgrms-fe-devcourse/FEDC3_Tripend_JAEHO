03e5b581ec90ca8b8435ffaed2f9afa2fca1a845
ksh-jsx <shkim787@naver.com>
2023-04-27 14:51:13 +0900

    refactor: user 디자인 수정 및 폴더링 수정

--- src/components/User/UserPageSidebar/style.ts
+++ src/components/User/UserPageSidebar/style.ts
@@ -1,18 +1,32 @@
 import styled from '@emotion/styled';
 
-const SideBlock = styled.div``;
-
 const Menu = styled.ul`
-  width: 200px;
+  width: 240px;
+  height: 120px;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   padding: 0;
+  background-color: var(--white);
+  box-shadow: 0px 4px 16px rgba(17, 34, 17, 0.05);
+  border-radius: 12px;
 `;
 
 const MenuItem = styled.li`
   list-style: none;
-  margin: 1rem;
-  font-size: 1rem;
+  margin: 0 20px;
+  padding: 16px 0;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 20px;
+  cursor: pointer;
+  border-bottom: 1px solid #d7e2ee;
+  /* &:hover {
+    background-color: var(--background-color);
+  } */
+  &:last-of-type {
+    border-bottom: 0;
+  }
 `;
 
-export { SideBlock, Menu, MenuItem };
+export { Menu, MenuItem };
--- src/components/User/UserPost/UserPosteItem/style.ts
+++ src/components/User/UserPost/UserPosteItem/style.ts
@@ -5,7 +5,8 @@ const PostItemContainer = styled.div`
   margin: 20px 10px;
   width: calc(100% - 20px);
   border-radius: 16px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 4px 16px rgba(17, 34, 17, 0.05);
+  color: var(--white);
 `;
 
 const PosterButton = styled.button`
@@ -38,4 +39,57 @@ const PosterDeleteButton = styled.button`
   }
 `;
 
-export { PostItemContainer, PosterButton, PosterDeleteButton };
+const PostTitle = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  right: 30%;
+  width: 50%;
+  bottom: 25%;
+  justify-content: space-between;
+  font-size: 0.8rem;
+  p {
+    margin-left: 30px;
+  }
+
+  @media (max-width: 768px) {
+    p {
+      font-size: 0.5rem;
+      font-weight: 200;
+      width: 100%;
+    }
+  }
+`;
+
+const PostButton = styled.div`
+  display: flex;
+  align-items: center;
+  width: 15%;
+  position: absolute;
+  right: 5%;
+  bottom: 30%;
+  color: #cccbc7;
+  button {
+    margin-right: 10px;
+  }
+`;
+
+const ImageItemContainer = styled.div`
+  width: 100px;
+  height: 80px;
+  margin: 10px;
+  border-radius: 16px;
+  background-color: #eee;
+  > img {
+    transition: opacity 0.4s ease-out;
+  }
+`;
+
+export {
+  PostItemContainer,
+  PosterButton,
+  PosterDeleteButton,
+  PostTitle,
+  PostButton,
+  ImageItemContainer,
+};

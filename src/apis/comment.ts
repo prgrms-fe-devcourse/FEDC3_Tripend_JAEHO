6773d3de10568bb75d5d4aef3b9a068ca07f5bf6
ksh-jsx <shkim787@naver.com>
2023-05-02 22:11:35 +0900

    refactor: api폴더 TS 마이그레이션

--- src/apis/comment.ts
+++ src/apis/comment.ts
@@ -1,7 +1,7 @@
 import { POST_URL } from '../utils/constants/post';
 import { authRequest } from './core';
 
-export const createComment = async (postId, comment) => {
+export const createComment = async (postId: string, comment: string) => {
   try {
     return await authRequest.post(POST_URL.CREATE_COMMENTS, {
       comment,

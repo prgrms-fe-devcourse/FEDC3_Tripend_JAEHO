06fc8225826ee722278300cd7b4a42de9cfe412a
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-05-11 14:54:45 +0900

    rename: 확장자 변경( js -> ts)

--- src/components/Post/PostCreate/UploadImage/index.tsx
+++ src/components/Post/PostCreate/UploadImage/index.tsx
@@ -1,7 +1,7 @@
 import { useCallback } from 'react';
 import { useRecoilState } from 'recoil';
 import { uploadImageState } from '@/recoil/uploadImageState';
-import { ImageFileInput } from '../AddPostForm/style';
+import { ImageFileInput } from '@/components/Post/PostCreate/AddPostForm/style';
 import { ERROR_MESSAGE, FILE } from '@/utils/constants/user';
 import uploadIcon from '/assets/upload.svg';
 import {

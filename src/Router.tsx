9694d5003cfe11e323f00d9ecccf13d1c564d77e
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-05-16 14:54:07 +0900

    rename: 확장자 변경 (js -> ts)

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,15 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Header from './pages/Header';
-import SigninPage from './pages/SigninPage';
-import SignupPage from './pages/SignupPage';
+import Header from '@/pages/Header';
+import SigninPage from '@/pages/SigninPage';
+import SignupPage from '@/pages/SignupPage';
 
-import HomePage from './pages/HomePage';
-//import MySettingPage from './pages/MySettingPage';
+import HomePage from '@/pages/HomePage';
 import MyHomePage from '@/pages/MyHomePage';
 
-import MissingPage from './pages/NotFound';
+import MissingPage from '@/pages/NotFound';
 
-import AuthUserRoute from './components/Auth/RequireAuth';
-import PostDetailPage from './pages/PostDetailPage';
+import AuthUserRoute from '@/components/Auth/RequireAuth';
+import PostDetailPage from '@/pages/PostDetailPage';
 
 const AppRouter = () => {
   return (
